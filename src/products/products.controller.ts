139a1b58281436a9ed492d2ca49eb5ad8e8be9b2
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-11-12 22:31:49 +0100

    POST,GET,PATCH,DELETE

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,58 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
   @Post()
-  addProduct(): any {}
+  addProduct(
+    //@Body() completeBody: { title: string; description: string; price: number },
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    //const { title, description, price } = completeBody;
+    //this.productsService.inserProduct(title, description, price);
+    const generatedId = this.productsService.inserProduct(
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getSingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodId: string) {
+    this.productsService.deleteProduct(prodId);
+    return null;
+  }
 }
